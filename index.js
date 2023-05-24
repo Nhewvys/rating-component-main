b9a3df43a3b5c6e64e5ae4f7b5b0464873a59ad6
Nhewvys Batista <nhewvys.silva@gmail.com>
2023-05-24 09:31:32 -0300

    js melhorado
    agora a pessoa pode escolher apenas um numero e se nenhum numero for escolhido o 5 sera enviado como valor padrão, adicionei responsividade para telas com 450px

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const btn = document.getElementById('submit');
 const rate = document.getElementById('rate');
 const voto = document.querySelector(".voto");
 const btns = document.querySelectorAll('.btn');
-let checar = document.getElementById('checar');
 
+//--------verifica a visibilidade do card--------
 btn.addEventListener("click", () => {
     thankYou.classList.toggle("hidden");
     mainContainer.classList.toggle("hidden");
@@ -15,9 +15,22 @@ rate.addEventListener("click", () => {
     thankYou.classList.toggle("hidden");
     mainContainer.classList.toggle("hidden");
 });
-  
+
+//----------botão clicado-------
 btns.forEach(function(btn) {
-    btn.addEventListener('click', function(){
-        voto.innerHTML = btn.innerHTML;
+    voto.innerHTML = 5;                 //define valor padrão
+    btns[4].classList.add("check");
+
+    btn.addEventListener('click', function() {
+      // Remover a classe 'check' de todos os botões
+      btns.forEach(function(btn) {
+        btn.classList.remove("check");
+      });
+  
+      // Adicionar a classe 'check' apenas ao botão atualmente clicado
+      this.classList.add("check");
+  
+      // Atualizar o conteúdo do elemento 'voto' com o texto do botão atualmente clicado
+      voto.innerHTML = this.innerHTML;
     });
 });
